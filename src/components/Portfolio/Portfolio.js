c7271c1f7bf9e786fc40a2eacb2a2a2a154e6770
AnassCyberDev <anasssssbenomar@gmail.com>
2024-09-14 20:27:57 +0100

    14/09/2024_first

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,6 +1,10 @@
 import React,{useEffect} from 'react'
 import { useNavigate,useLocation } from 'react-router-dom'
-
+import bfdrive from '../Styling/img/bfdrive.jpeg'
+import machango from '../Styling/img/machango.jpeg'
+import mikaza from '../Styling/img/miKaza.jpeg'
+import bfdriveMobile from '../Styling/img/bfdrive_mobile.jpeg'
+import portfolio from "../Styling/img/portfolio.jpeg"
 export const Portfolio = ({setPath}) => {
 
     const location=useLocation()
@@ -37,40 +41,40 @@ setPath(pageName(location.pathname))
         <h1 className="text-color-prim text-center">Portfolio</h1>
     </div>
     <div className="col-12 text-center text-white">
-        <h4 className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+        <h4 className="description">Mes Réalisations Récentes</h4>
     </div>
     </div>
     <div className="row mt-5">
     <div className="col-md-4 p-3">
-        <a href="#">
-            <img src="https://slimhamdi.net/istanbul/demos/img/projects/project-1.jpg" alt="link to work" style={{width: "100%",borderRadius: "10px"}}/>
+        <a target="_blank" href="https://bfdrive.ma">
+            <img src={bfdrive} alt="link to work" style={{width: "100%",borderRadius: "10px"}}/>
         </a>
     </div>
     <div className="col-md-4 p-3">
-        <a href="#">
-            <img src="https://slimhamdi.net/istanbul/demos/img/projects/project-2.jpg" alt="link to work" style={{width: "100%",borderRadius: "10px"}}/>
+        <a target="_blank" href="https://play.google.com/store/apps/details?id=com.bfdrive.mcar">
+            <img src={bfdriveMobile} alt="link to work" style={{width: "100%",borderRadius: "10px"}}/>
         </a>
     </div>
     <div className="col-md-4 p-3">
-        <a href="#">
-            <img src="https://slimhamdi.net/istanbul/demos/img/projects/project-3.jpg" alt="link to work" style={{width: "100%",borderRadius: "10px"}}/>
+        <a target="_blank" href="https://mikaza-renov.fr">
+            <img src={mikaza} alt="link to work" style={{width: "100%",borderRadius: "10px"}}/>
         </a>
     </div>
     <div className="col-md-4 p-3">
-        <a href="#">
-            <img src="https://slimhamdi.net/istanbul/demos/img/projects/project-4.jpg" alt="link to work" style={{width: "100%",borderRadius: "10px"}}/>
+        <a target="_blank" href="/">
+            <img src={portfolio} alt="link to work" style={{width: "100%",borderRadius: "10px"}}/>
         </a>
     </div>
     <div className="col-md-4 p-3">
-        <a href="#">
-            <img src="https://slimhamdi.net/istanbul/demos/img/projects/project-5.jpg" alt="link to work" style={{width: "100%",borderRadius: "10px"}}/>
+        <a target="_blank" href="https://matchango-anasscyberdevs-projects.vercel.app/fr/demo">
+            <img src={machango} alt="link to work" style={{width: "100%",borderRadius: "10px"}}/>
         </a>
     </div>
-    <div className="col-md-4 p-3">
+    {/* <div className="col-md-4 p-3">
         <a href="#">
             <img src="https://slimhamdi.net/istanbul/demos/img/projects/project-6.jpg" alt="link to work" style={{width: "100%",borderRadius: "10px"}}/>
         </a>
-    </div>
+    </div> */}
     </div>
 </div>
 
