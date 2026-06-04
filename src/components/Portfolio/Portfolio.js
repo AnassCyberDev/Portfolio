0ad8521e2a08da81327b12b9ce91c47781490a66
anasscybersec <anasscybersec@gmail.com>
2026-06-04 12:17:49 +0100

    add dentiste-web

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,11 +1,57 @@
 import React,{useEffect} from 'react'
 import { useLocation } from 'react-router-dom'
-import bfdrive from '../Styling/img/bfdrive.jpeg'
-import machango from '../Styling/img/machango.jpeg'
-import mikaza from '../Styling/img/miKaza.jpeg'
-import bfdriveMobile from '../Styling/img/bfdrive_mobile.jpeg'
-import portfolio from "../Styling/img/portfolio.jpeg"
-import toagri from "../Styling/img/toagri.png"
+import bfdrive from '../Styling/img/bfdrive.webp'
+import machango from '../Styling/img/machango.webp'
+import mikaza from '../Styling/img/miKaza.webp'
+import bfdriveMobile from '../Styling/img/bfdrive_mobile.webp'
+import dentiste from "../Styling/img/dentiste.webp"
+import toagri from "../Styling/img/toagri.webp"
+
+const projects=[
+    {
+        id:1,
+        name:"BF Drive",
+        description:"A web application for managing and sharing files.",
+        image:bfdrive,
+        link:"https://bfdrive.ma"
+    },  
+    {
+        id:2,
+        name:"BF Drive Mobile",
+        description:"A web application for managing and sharing files.",
+        image:bfdriveMobile,
+        link:"https://play.google.com/store/apps/details?id=com.bfdrive.mcar"
+    },
+    {
+        id:3,
+        name:"Machango",
+        description:"A web application for managing and sharing files.",
+        image:machango,
+        link:"https://machango.vercel.app/"
+    },
+    {
+        id:4,
+        name:"Mikaza",
+        description:"A web application for managing and sharing files.",
+        image:mikaza,
+        link:"https://mikaza-renov.vercel.app/"
+    },
+    {
+        id:5,
+        name:"dentiste",
+        description:"A web application for managing and sharing files.",
+        image:dentiste,
+        link:"https://dentiste-ennouira.ma/"
+    },
+    {
+        id:6,
+        name:"Toagri",
+        description:"A web application for managing and sharing files.",
+        image:toagri,
+        link:"https://d3cipv40mmghiw.cloudfront.net/"
+    },  
+  
+]
 export const Portfolio = ({setPath}) => {
 
     const location=useLocation()
@@ -46,7 +92,14 @@ setPath(pageName(location.pathname))
     </div>
     </div>
     <div className="row mt-5">
-    <div className="col-md-4 p-3">
+    {projects.map((project)=>(
+        <div className="col-md-4 p-3" key={project.id}>
+            <a target="_blank" rel="noreferrer" href={project.link}>
+                <img src={project.image} alt="link to work" style={{width: "100%",borderRadius: "10px"}}/>
+            </a>
+        </div>
+    ))}
+    {/* <div className="col-md-4 p-3">
         <a target="_blank" rel="noreferrer" href="https://bfdrive.ma">
             <img src={bfdrive} alt="link to work" style={{width: "100%",borderRadius: "10px"}}/>
         </a>
@@ -75,7 +128,7 @@ setPath(pageName(location.pathname))
         <a target="_blank" rel="noreferrer" href="https://d3cipv40mmghiw.cloudfront.net/">
             <img src={toagri} alt="link to work" style={{width: "100%",borderRadius: "10px"}}/>
         </a>
-    </div>
+    </div> */}
     {/* <div className="col-md-4 p-3">
         <a href="#">
             <img src="https://slimhamdi.net/istanbul/demos/img/projects/project-6.jpg" alt="link to work" style={{width: "100%",borderRadius: "10px"}}/>
